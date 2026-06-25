79fa8b60f60911201735dd86b5bc793e0e578ab0
pravach <pravachkumar@gmail.com>
2026-06-25 20:10:35 +0530

    Time: 0 ms (100%), Space: 59.2 MB (12.14%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -2,10 +2,6 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getConcatenation = function(nums) {
-    let ans=nums.concat(nums);
-    return ans;
-    
-};
-let nums = [1,2,1];
-console.log(getConcatenation(nums));
+function getConcatenation(nums) {
+    return [...nums, ...nums];
+}
