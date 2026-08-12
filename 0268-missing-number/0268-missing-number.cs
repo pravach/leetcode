835998d3b2702ef99e2ccaa23f2c070e6ed4a858
pravach <pravachkumar@gmail.com>
2026-08-12 09:50:47 +0530

    Time: 0 ms (100%), Space: 47 MB (78.53%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        foreach (int num in nums)
+        {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
