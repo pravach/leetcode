5380861a56a64684c0aae450bf019018569ffa2e
pravach <pravachkumar@gmail.com>
2026-09-19 09:43:12 +0530

    Time: 0 ms (100%), Space: 47.6 MB (30.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MajorityElement(int[] nums)
+{
+    int candidate = 0;
+    int count = 0;
+
+    foreach (int num in nums)
+    {
+        if (count == 0)
+        {
+            candidate = num;
+        }
+
+        if (num == candidate)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+}
